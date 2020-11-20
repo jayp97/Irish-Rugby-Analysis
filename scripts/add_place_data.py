98b5fbce49db89e68d2ca738c446c823bb1f2baa
Jay Pinion <jaypinion97@gmail.com>
2020-11-20 14:35:23 +0000

    Created a function that can scan the raw data and determine if it contains a county name. I will now improve this code to scan for village/town/city as well.

---
+++ scripts/add_place_data.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import re 
+import csv
+# import ogr
+
+# Now that I have the column of raw player data I need to extract the place data from it. 
+# Essentially where is the player from, town/village/city, county, Province, Country, Provided all this data is available. 
+
+rawName = csv.reader(open('raw/raw-player-data.csv','r'))
+countyList = []
+
+for row in rawName:
+    counties = csv.reader(open('raw/irish-counties-list.csv','r'))
+    if row[4] == "Raw-Birth-Data":
+        print('no')
+        continue
+
+    else: 
+        for county in counties:
+            print(county[0])
+            print(row[4])
+                
+            if county[0] in row[4]:
+                print(county[0])
+                countyList.append(county[0])
+                
+            else: 
+                print('does not contain')
+                
+print(countyList) 
+
